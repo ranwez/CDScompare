f755f9f63e64315b8ea6332080ea7eae370135f7
Vetea Jacot <veteajacot64@gmail.com>
2024-05-15 09:20:25 +0200

    Finished creating the new version, it... almost works. All tests are clear except diff-start-before_test and diff-start-after_test

--- script/main.py
+++ script/main.py
@@ -37,13 +37,11 @@ def get_gff_borders(path, debug, verbose):
     locus_id = "" # the current gene being analyzed
     
     for l in file:
-        
-        print(l)
 
         # if we encounter a new gene, we get its ID and create a key in 'borders' with a basic list
         if str(l.split("\t")[2]) == "gene": 
             
-            locus_id = l.split("\t")[8].split("")[0].split("\n")[0][3:]
+            locus_id = l.split("\t")[8].split("\n")[0][3:]
             
             # the locus list is intialised with a strand number of '1', which is the corrected if necessary
             borders[locus_id] = [1, []]
@@ -90,10 +88,10 @@ def get_area_bounds(ref, alt):
         # we add the next coordinate to the result list
         bounds.append(min(ref[i],alt[j]))
         
-        if(i==bounds[-1]):
+        if(ref[i] == bounds[-1]):
             i+=1
         
-        if(j==bounds[-1]):
+        if(alt[j] == bounds[-1]):
             j+=1
             
     return bounds
@@ -121,7 +119,7 @@ def is_in_cds(cds_bounds, area_bounds):
         ub= area_bounds[i+1]
 
         # if the current CDS is included in the area bounds, 'True' is used to index it in the return list, else 'False'
-        if(current_cds[0]<=lb[0] and lb[1]<=current_cds[1]):
+        if(current_cds[0]<=lb and ub<=current_cds[1]):
             in_cds.append(True)
         else:
             in_cds.append(False)
@@ -157,19 +155,16 @@ def compare_loci(ref, alt, debug, verbose):
     # we retrieve the bounds of all areas delimited by all the CDS coordinates of both border lists
     area_bounds = get_area_bounds(ref, alt)
     
-    # we retrieve the list indicating the areas which include or not a CDS for bot hannotations
+    # we retrieve the list indicating the areas which include or not a CDS for both annotations
     ref_in_CDS = is_in_cds(ref, area_bounds)
     alt_in_CDS = is_in_cds(alt, area_bounds)
-
-    # initialisation of the variables indicating if the annotations have a CDS in the current area, and of the variables keeping track of the current codon position
-    ref_in_CDS = True if (ref[0]==area_bounds[0]) else False
-    alt_in_CDS = True if (alt[0]==area_bounds[0]) else False
     
-    if( ref_in_CDS):
-        codon_position_ref=0
-    if( alt_in_CDS):
-        codon_position_alt=0
-  
+    if debug:
+        print("Ref_in_CDS = " + str(ref_in_CDS))
+        print("Alt_in_CDS = " + str(alt_in_CDS))
+    
+    codon_position_ref=0
+    codon_position_alt=0
     
     comparison = [0,0] # return list. First value is mismatches, second value is identities
     bound_id=0
@@ -179,12 +174,24 @@ def compare_loci(ref, alt, debug, verbose):
     for bound in area_bounds[1:] :
         
         if(prev_bounds>0):
-            
+                
+            if debug:
+                
+                print("\n")
+                print("Bound = " + str(bound))
+                print("Prev_bound = " + str(prev_bounds))
+                print("Bound_id = " + str(bound_id))
+                print("Codon_position_ref = " + str(codon_position_ref))
+                print("Codon_position_alt = " + str(codon_position_alt))
+                print("Ref_in_CDS[bound_id] = " + str(ref_in_CDS[bound_id]))
+                print("Alt_in_CDS[bound_id] = " + str(alt_in_CDS[bound_id]))
+                print("\n")
+                
             # if both annotations are in a CDS and have the same codon position, then all codon positions for the rest of the area will be identical, so we add the length of the area to the second value of the comparison list and update both codon positions
             if(ref_in_CDS[bound_id] and alt_in_CDS[bound_id] and codon_position_alt==codon_position_ref):
                 comparison[1]+=bound-prev_bounds
                 codon_position_alt = (codon_position_alt + (bound-prev_bounds))%3 
-                codon_position_ref = (codon_position_ref + (bound-prev_bounds))%3 
+                codon_position_ref = (codon_position_ref + (bound-prev_bounds))%3
                 
             # if both annotations are in a CDS but don't have the same codon position, then all codon positions for the rest of the area will be different, so we add the length of the are to the first value of the comparison list and update both codon positions
             elif(ref_in_CDS[bound_id] and alt_in_CDS[bound_id] and codon_position_alt!=codon_position_ref):
@@ -207,122 +214,15 @@ def compare_loci(ref, alt, debug, verbose):
        
         prev_bounds=bound
         
+        bound_id += 1
+        
+    if verbose:
+        print(f"Result of the comparison of the locus : {str(comparison)} ( {comparison[1]} matches and {comparison[0]} mismatches )\n")
+        
     return comparison
 
 
-## This function uses the variables created by the function @see compare_loci() to compute the comparison matrix of the comparison of the two areas delimited by lower and upper. It takes a comparison matrix as input to add each area comparison to the global locus comparison matrix.
-#
-# @see compare_loci()
-#
-# @param comp_matrix The initial comparison matrix in which to add the result of the area's nucleotide comparisons. It is a list of lists of dimensions (4,4).
-#
-# @param upper The upper bound on the genome of the locus area to consider
-#
-# @param lower The lower bound on the genome of the locus area to consider
-#
-# @param codon_position_ref The codon position of the next CDS nucleotide of the reference annotation
-#
-# @param codon_position_alt The codon position of the next CDS nucleotide of the alternative annotation
-#
-# @param ref_in_CDS Boolean indicating if the area includes a reference annotation's CDS
-#
-# @param alt_in_CDS Boolean indicating if the area includes an alternative annotation's CDS
-#
-# @param debug If True, triggers display of many messages intended for debugging the program
-#
-# @param verbose If True, triggers display of more information messages. Default is 'False'
-#
-# @return Returns the initial comparison matrix (list of lists of dimensions (4,4)) with the area's comparison results added to it
-#
-def increment_matrix(comp_matrix, upper, lower, codon_position_ref, codon_position_alt, ref_in_CDS, alt_in_CDS, debug, verbose):
-    
-    # initialisation of the variables indicating in which line (corresponding to the reference) and in which column (corresponding to the alternative) to increment the nucleotide count
-    ref_nucl = 0
-    alt_nucl = 0
-    
-    for i in range(0, upper-lower):
-        
-        # if the area includes a reference annotation's CDS...
-        if ref_in_CDS:
-            
-            # then the line in which to increment the nucleotide count is indicated by the current reference codon position
-            ref_nucl = codon_position_ref
-            
-        else:
-            
-            # if it does not, then we increment in the line '0'
-            ref_nucl = 0
-            
-        # if the area includes an alternative annotation's CDS...
-        if alt_in_CDS:
-            
-            # then the column in which to increment the nucleotide count is indicated by the current alternative codon position
-            alt_nucl = codon_position_alt
-            
-        else:
-            
-            # if it does not, then we increment in the column '0'
-            alt_nucl = 0
-            
-        # we increment the corresponding 'cell'
-        comp_matrix[ref_nucl][alt_nucl] += 1
-        
-        # we increment the codon positions if we are in an annotation's CDS
-        if ref_in_CDS:
-            
-            codon_position_ref += 1
-            
-            if codon_position_ref == 4:
-                
-                codon_position_ref = 1
-                
-        if alt_in_CDS:
-            
-            codon_position_alt += 1
-            
-            if codon_position_alt == 4:
-                
-                codon_position_alt = 1
-    
-    return comp_matrix, codon_position_ref, codon_position_alt
-
-
-## This function computes the identity of structure strings of two loci from the given comparison 
-# matrix (list of lists). Returns the identity as a percentage.
-#
-# @param matrix The comparison matrix returned by pair_vector_comparison()
-#
-# @param debug If True, triggers display of many messages intended for debugging the program
-#
-# @param verbose If True, triggers display of more information messages. Default is 'False'
-#
-# @see pair_vector_comparison()
-#
-# @return Returns the identity of the two structure strings describded by the matrix
-#
-def matrix_to_identity(matrix, debug, verbose):
-    
-    # number of string positions for which 'caracters' were found to be identical. 'matrix[0][0]' is not taken into account so as to reduce the impact of large introns correctly prédicted
-    match = matrix[1][1] + matrix[2][2] + matrix[3][3]
-    
-    if verbose:
-        print("matching codon positions in both annotations = " + str(match))
-
-    # number of string positions for which 'caracters' were found to be identical
-    mismatch = matrix[0][1] + matrix[0][2] + matrix[0][3] + matrix[1][0] + matrix[1][2] + matrix[1][3] + matrix[2][0] + matrix[2][1] + matrix[2][3] + matrix[3][0] + matrix[3][1] + matrix[3][2]
-    
-    if verbose:
-        print("mismatching codon positions in both annotations = " + str(mismatch) + "\n")
-        
-    result = round( float(match) / (float(match) + float(mismatch) ) * 100 , 1 )
-    
-    if verbose:
-        print("Proportion of matching codon positions in both annotations = " + str(result) + "\n")
-
-    return(result)
-
-
-## Main function of this program. Given a reference and alternative path, gets the corresponding GFF files and compares the two annotations
+## Main function of this program. Given a reference and alternative path, gets the corresponding GFF files and compares the two annotations to return their structure's identity level
 #
 # @param ref_path Path of the GFF file describing the reference annotation
 #
@@ -352,12 +252,11 @@ def annotation_comparison(ref_path, alt_path, debug, verbose):
         
             # construct the comparison matrix for the reference and alternative locus annotation
             if verbose:
-                print("Creation of comparison matrix of locus " + locus + "\n")
-            comp_mat = compare_loci(ref_annotations[locus][1], alt_annotations[locus][1], debug, verbose)
+                print("\nStarting the comparison of the locus " + locus + "\n")
+            comp_res = compare_loci(ref_annotations[locus][1], alt_annotations[locus][1], debug, verbose)
             
             # compute identity from the matrix and index it in the identities dictionary
-            ident = matrix_to_identity(comp_mat, debug, verbose)
-            identities[locus] = ident
+            identities[locus] = round( float(comp_res[1]) / (float(comp_res[1]) + float(comp_res[0]) ) * 100 , 1 )
             
             if verbose:
                 print("Finished computing the identity of the annotations of locus " + locus + "\n")
@@ -368,7 +267,7 @@ def annotation_comparison(ref_path, alt_path, debug, verbose):
             identities[locus] = 0.0
             
             if verbose:
-                print("Locus predicted on a different strand from the reference in the alternative. Identity : 0%\n")
+                print("\nLocus predicted on a different strand from the reference in the alternative. Identity : 0%\n")
             
     return identities
 
@@ -412,6 +311,7 @@ def main():
     # call of the annotation_comparison function
     comparison = annotation_comparison(ref_path, alt_path, debug, verbose)
     
+    print("Result of the comparison of all loci of both annotations :\n")
     pprint.pprint(comparison)
     
 if __name__ == "__main__":
