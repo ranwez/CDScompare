667661f9f20eb86d1157544f3305f1a399a15473
Johanna Girodolle <38213007+JohGi@users.noreply.github.com>
2024-10-07 15:03:15 +0200

    Removed spaces around commas in the output
    Removed spaces around commas in the output csv and fixed a missing f in print(f"...) for an error message

--- script/CDScompR.py
+++ script/CDScompR.py
@@ -7,9 +7,9 @@ Created on Thu May  2 11:57:29 2024
 """
 
 ##@package CDScompR
-# This script is used to compute the distance between two structural 
-# annotations of a same genome, one reference and one alternative annotation. 
-# It expects as input the paths to the annotation files (in GFF format), 
+# This script is used to compute the distance between two structural
+# annotations of a same genome, one reference and one alternative annotation.
+# It expects as input the paths to the annotation files (in GFF format),
 # displays the computed distances between all annotation pairs, and creates
 # a results CSV file detailing the loci comparisons between the annotations
 
@@ -24,19 +24,19 @@ import read_files as rf
 import pre_comparison as pc
 import comparison as comp
 
-## This function writes to a new 'results.csv' file the results of the 
-# annotation comparison retrieved from the identities dictionary returned by 
+## This function writes to a new 'results.csv' file the results of the
+# annotation comparison retrieved from the identities dictionary returned by
 # the annotation_match function
 #
 # @see annotation_match()
 #
-# @param results A dictionary of list of list of dictionaries containing 
+# @param results A dictionary of list of list of dictionaries containing
 # results of the annotation comparison, as returned by annotation_match
 #
-# @param alt_name String indicating the name of the original alternative 
+# @param alt_name String indicating the name of the original alternative
 # annotation file
 #
-# @param verbose If True, triggers display of more information messages. 
+# @param verbose If True, triggers display of more information messages.
 # Default is 'False'
 #
 # @see annotation_match()
@@ -46,23 +46,23 @@ def write_results(all_results, alt_name, verbose=False):
     # annotation origin of each locus in the results for all chromosomes
     # (first value : both,  second value : reference,  third value : alternative)
     final_locus_annot = [0, 0, 0]
-    
+
     # try to open the results file (named after the alternative file name)
     try:
         results_file = open(f"./results/{alt_name}.csv", "w")
     except FileNotFoundError:
         os.mkdir("./results/") # create 'results' subdirectory
         results_file = open(f"./results/{alt_name}.csv", "w")
-        
-    results_file.write("Chromosome, Cluster name, Reference locus,Alternative locus,Comparison matches,Comparison mismatches,Identity score (%),Reference start, Reference end, Alternative start, Alternative end, Reference mRNA, Alternative mRNA, Exon_intron (EI) non-correspondance zones, Reading frame (RF) non-correspondance zones, Exon_Intron (EI) mismatches, Reading Frame (RF) mismatches, reference mRNA number, alternative mRNA number\n")
-    
+
+    results_file.write("Chromosome,Cluster name,Reference locus,Alternative locus,Comparison matches,Comparison mismatches,Identity score (%),Reference start,Reference end,Alternative start,Alternative end,Reference mRNA,Alternative mRNA,Exon_intron (EI) non-correspondance zones,Reading frame (RF) non-correspondance zones,Exon_Intron (EI) mismatches,Reading Frame (RF) mismatches,reference mRNA number,alternative mRNA number\n")
+
     for dna_mol, results in all_results.items():
         print(f"\n**************** Results for chromosome {dna_mol} ****************\n")
-            
+
         # annotation origin of each locus in the results
         # (first value: both,  second: reference,  third: alternative)
         locus_initial_annot = [0,0,0]
-        
+
         for cluster in results:
             for loc in cluster:
                 if loc['mismatch zones'] not in ["_", "?"]:
@@ -73,49 +73,49 @@ def write_results(all_results, alt_name, verbose=False):
                         mismatch_EI += "[" + str(loc['mismatch zones'][0][i]) + "//" + str(loc['mismatch zones'][0][i+1]) + "] "
                     for i in range(0, len(loc['mismatch zones'][1]), 2):
                         mismatch_RF += "[" + str(loc['mismatch zones'][1][i]) + "//" + str(loc['mismatch zones'][1][i+1]) + "] "
-                    
+
                 else:
                     mismatch_EI = loc['mismatch zones']
                     mismatch_RF = loc['mismatch zones']
-                
-                # if no comparison was done for the loci, write '~' instead of 
+
+                # if no comparison was done for the loci, write '~' instead of
                 # the comparison values
                 if loc['mismatch/match'] == []:
                     print(f"{loc['cluster name']}\t\t{loc['reference']}\t\t{loc['alternative']}\t\t\t_\t\t\t\t_")
-                    results_file.write(f"{dna_mol}, {loc['cluster name']},{loc['reference']},{loc['alternative']},_,_,{loc['identity']},{loc['reference start']},{loc['reference end']},{loc['alternative start']},{loc['alternative end']}, {loc['reference mRNA']}, {loc['alternative mRNA']}, _, _, _, _, {loc['reference mRNA number']}, {loc['alternative mRNA number']}\n")       
+                    results_file.write(f"{dna_mol},{loc['cluster name']},{loc['reference']},{loc['alternative']},_,_,{loc['identity']},{loc['reference start']},{loc['reference end']},{loc['alternative start']},{loc['alternative end']},{loc['reference mRNA']},{loc['alternative mRNA']},_,_,_,_,{loc['reference mRNA number']},{loc['alternative mRNA number']}\n")
                     if loc['reference'] == '~':
                         locus_initial_annot[2] += 1
-                    else:                       
+                    else:
                         locus_initial_annot[1] += 1
                 else:
                     print(f"{loc['cluster name']}\t\t{loc['reference']}\t\t{loc['alternative']}\t\t\t{loc['mismatch/match']}\t\t\t\t{loc['identity']}%")
-                    results_file.write(f"{dna_mol}, {loc['cluster name']},{loc['reference']},{loc['alternative']},{loc['mismatch/match'][0]},{loc['mismatch/match'][1]+loc['mismatch/match'][2]},{loc['identity']},{loc['reference start']},{loc['reference end']},{loc['alternative start']},{loc['alternative end']}, {loc['reference mRNA']}, {loc['alternative mRNA']}, {mismatch_EI}, {mismatch_RF}, {loc['mismatch/match'][1]}, {loc['mismatch/match'][2]}, {loc['reference mRNA number']}, {loc['alternative mRNA number']}\n")
+                    results_file.write(f"{dna_mol},{loc['cluster name']},{loc['reference']},{loc['alternative']},{loc['mismatch/match'][0]},{loc['mismatch/match'][1]+loc['mismatch/match'][2]},{loc['identity']},{loc['reference start']},{loc['reference end']},{loc['alternative start']},{loc['alternative end']},{loc['reference mRNA']},{loc['alternative mRNA']},{mismatch_EI},{mismatch_RF},{loc['mismatch/match'][1]},{loc['mismatch/match'][2]},{loc['reference mRNA number']},{loc['alternative mRNA number']}\n")
                     locus_initial_annot[0] += 2
-                    
+
         print(f"\nNumber of loci of chromosome {dna_mol}:\n- found in both annotations : {locus_initial_annot[0]}\n- found only in the reference : {locus_initial_annot[1]}\n- found only in the alternative : {locus_initial_annot[2]}\n")
         # add locus origin counts of the chromosome to final counts for all
         final_locus_annot = [sum(x) for x in zip(final_locus_annot, locus_initial_annot)]
-        
+
     results_file.close()
-    
+
     # write to a txt file the collected origins of loci
     results_file = open(f"./results/{alt_name}.txt", "w")
     results_file.write(f"\nNumber of loci (whole data):\n- found in both annotations : {final_locus_annot[0]}\n- found only in the reference : {final_locus_annot[1]}\n- found only in the alternative : {final_locus_annot[2]}\n")
     results_file.close()
-    
+
     # display in the standard ouput the loci origins
     print(f"\nNumber of loci (whole data):\n- found in both annotations : {final_locus_annot[0]}\n- found only in the reference : {final_locus_annot[1]}\n- found only in the alternative : {final_locus_annot[2]}\n")
-    
 
-## Main function of this program. Given a reference and alternative path, 
-# gets the corresponding GFF files and compares the two annotations to return 
+
+## Main function of this program. Given a reference and alternative path,
+# gets the corresponding GFF files and compares the two annotations to return
 # their information about their loci's comparison
 #
 # @param ref_path Path of the GFF file describing the reference annotation
 #
 # @param alt_path Path of the GFF file describing the aternative annotation
 #
-# @param verbose If True, triggers display of more information messages. 
+# @param verbose If True, triggers display of more information messages.
 # Default is 'False'
 #
 # @param create_strings Boolean indicating wether to use the 'old' comparison
@@ -129,7 +129,7 @@ def write_results(all_results, alt_name, verbose=False):
 #
 # @see old_compare_loci()
 #
-# @return Returns a list of lists of dictionaries describing the 
+# @return Returns a list of lists of dictionaries describing the
 # comparison of the structure identity between the loci of each annotation
 def annotation_comparison(ref_path, alt_path, verbose=False, create_strings=False, exon_mode=False):
 
@@ -144,37 +144,37 @@ def annotation_comparison(ref_path, alt_path, verbose=False, create_strings=Fals
             print(f"Constructing the locus order list of the chromosome {dna_mol}")
         locus_order = pc.annotation_sort(ref_annotations[dna_mol], alt_annotations[dna_mol], verbose)
         all_locus_order[dna_mol] = locus_order
-    
+
     # construct clusters of overlapping loci
     all_cluster_list = {}
     for dna_mol in all_locus_order.keys():
         cluster_list = pc.construct_clusters(ref_annotations[dna_mol], alt_annotations[dna_mol], all_locus_order[dna_mol], verbose)
         all_cluster_list[dna_mol] = cluster_list
-    
+
     all_results = {}
     for dna_mol in all_cluster_list.keys():
         results = []
         for cluster_id, cluster in all_cluster_list[dna_mol].items():
             results.append(comp.annotation_match(cluster, create_strings, verbose))
         all_results[dna_mol] = results
-        
+
     alt_name = (os.path.basename(alt_path)).split(".")[0]
-        
+
     print("\nCluster name\tReference_Locus\t\tAlternative_Locus\t\tComparison[match/mismatch_EI/mismatch_RF]\t\tIdentity_Score\n")
     write_results(all_results, alt_name, verbose)
-    
+
     return all_results
-    
+
 
 def usage():
-    
+
     # displayed when '-h' or '--help' is given, or when an invalid script
     # call happens
     print("Syntax : path/to/CDScompR.py [ -h/--help -v/--verbose -o/--old_version ] [ -r/--reference <reference_file_path> ] [ -a/--alternative <alternative_file_path> ] ")
-    
+
 
 def main():
-    
+
     # we retrieve all script call options
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hvoer:a:", ["help", "verbose", "old_version", "exon-mode", "reference=", "alternative="])
@@ -182,24 +182,24 @@ def main():
         print(err)
         usage()
         sys.exit(2)
-        
-    # initialisation of the display parameters 
+
+    # initialisation of the display parameters
     #
-    # verbose: display messages indicating which step the program is currently 
+    # verbose: display messages indicating which step the program is currently
     # on, intended to be used when the program is called directly (not
     # integrated in a pipeline or workflow)
     verbose = False
-    
-    # boolean indicating which version of the file reading function to use: 
+
+    # boolean indicating which version of the file reading function to use:
     # False = read coding sequences (CDS) from the given files, True = read
     # the exons from the given files
-    exon_mode = False  
-    
-    # boolean indicating which version of the program to use: False = new 
-    # version without any structure string creation, True = 'old' version with 
+    exon_mode = False
+
+    # boolean indicating which version of the program to use: False = new
+    # version without any structure string creation, True = 'old' version with
     # creation of structure strings to compare the loci of the annotations
     create_strings = False
-    
+
     # we retrieve the values given for each parameter
     for o, a in opts:
         if o in ("-h", "--help"):
@@ -217,12 +217,10 @@ def main():
             alt_path = a
         else:
             assert False, "unhandled option"
-            
+
     # call of the annotation_comparison function
     return annotation_comparison(ref_path, alt_path, verbose, create_strings, exon_mode)
-    
+
 if __name__ == "__main__":
     #annotation_comparison("../data/real_data/annot_best.gff", "../data/real_data/TRITD_clean.gff3", False, False, False)
     main()
-    
-    
--- script/python_util/read_files.py
+++ script/python_util/read_files.py
@@ -12,225 +12,225 @@ import re
 import locus as lc
 
 
-## This function retrieves and returns the id of the structure described from 
+## This function retrieves and returns the id of the structure described from
 # a line read from a GFF file.
 #
 # @param parsed_line The list of the line read from the file parsed through its
 # tabulations (string 'split' method)
 #
 # @param delim Instance of class 'Pattern' ('re' library) compiled with the
-# regular expression selecting all characters from the start of the line to 
+# regular expression selecting all characters from the start of the line to
 # the first delimiter character
 #
-# @param verbose If True, triggers display of more information messages. 
+# @param verbose If True, triggers display of more information messages.
 # Default is 'False'
 #
 # @remark This function expects the file to be in GFF format
 #
 # @returns Returns the id of the structure described by the line
 def get_structure_id(parsed_line, delim, verbose=False):
-    
+
     # try to retrieve the last column, else return an error
-    try: 
+    try:
         last_col = parsed_line[8]
     except IndexError:
         print("\nFile is not in GFF format (no ninth column)")
         sys.exit(1)
-    
+
     # retrieve the id with the rest of the column text (commentaries)
     try:
         id_and_rest = last_col.split("=")[1]
     except IndexError:
         print("\nNo ID field found in last column (ninth column)")
         sys.exit(1)
-    
+
     structure_id = delim.findall(id_and_rest)[0]
-        
+
     return structure_id
 
 
-## This function retrieves and returns the parent id of the structure 
+## This function retrieves and returns the parent id of the structure
 # described from a line read from a GFF file.
 #
 # @param parsed_line The list of the line read from the file parsed through its
 # tabulations (string 'split' method)
 #
 # @param delim Instance of class 'Pattern' ('re' library) compiled with the
-# regular expression selecting all characters from the start of the line to 
+# regular expression selecting all characters from the start of the line to
 # the first delimiter character
 #
-# @param verbose If True, triggers display of more information messages. 
+# @param verbose If True, triggers display of more information messages.
 # Default is 'False'
 #
-# @remark This function expects the file to be in GFF format, and the 
+# @remark This function expects the file to be in GFF format, and the
 # parent id field to come after the id field
 #
 # @returns Returns the id of the parent of the structure described by the line
 def get_parent_id(parsed_line, delim, verbose=False):
-    
+
     # try to retrieve the last column, else return an error
-    try: 
+    try:
         last_col = parsed_line[8]
     except IndexError:
         print("\nFile is not in GFF format (no ninth column)")
         sys.exit(1)
-    
+
     # retrieve the id with the rest of the column text (commentaries)
     try:
         id_and_rest = last_col.split("=")[2]
     except IndexError:
         print("\nNo parent ID field found in last column (ninth column)")
         sys.exit(1)
-    
+
     structure_id = ""
-    
+
     structure_id = delim.findall(id_and_rest)[0]
-        
+
     return structure_id
 
 
-## This function expects a string corresponding to the file path of the GFF 
-# file to read, and returns a dictionary of dictionaries of instances of the 
-# class 'Locus', detailing all the relevant information for the gene and 
+## This function expects a string corresponding to the file path of the GFF
+# file to read, and returns a dictionary of dictionaries of instances of the
+# class 'Locus', detailing all the relevant information for the gene and
 # its mRNAs for each chromosome and DNA strand (direct/reverse)
 #
 # @see Locus
 #
 # @param path Path of the file to read
 #
-# @param verbose If True, triggers display of more information messages. 
+# @param verbose If True, triggers display of more information messages.
 # Default is 'False'
 #
 # @param exon_mode Boolean indicating if the main comparison structures read
 # from the file should be coding sequences (CDS, False) or exons (True).
 # Default is 'False' (CDS comaprison)
 #
-# @remark If the parent ID of a CDS does not match the ID of the previous 
-# mRNA (indicating an incorrect file structure), an entry is added to a 'log' 
+# @remark If the parent ID of a CDS does not match the ID of the previous
+# mRNA (indicating an incorrect file structure), an entry is added to a 'log'
 # file but the function is not interrupted
 #
-# @return Returns a dictionary of dictionaries of instances of the class 
-# 'Locus', containing for each chromosome and DNA strand the information of 
-# the CDS borders of each mRNA of the gene, the start and end coordinates, the 
+# @return Returns a dictionary of dictionaries of instances of the class
+# 'Locus', containing for each chromosome and DNA strand the information of
+# the CDS borders of each mRNA of the gene, the start and end coordinates, the
 # DNA strand on which the gene is predicted, and the locus ID
 def get_gff_borders(path, verbose=False, exon_mode=False):
-    
+
     # specifiy the desired main structure to be read
     if exon_mode:
         exon_or_cds = "exon"
     else:
         exon_or_cds = "CDS"
-    
-    try:    
+
+    try:
         file = open(path, "r") # the file to read
     except FileNotFoundError:
         print(f"\nget_gff_borders() function error : file '{path}' does not exist or cannot be read from\n")
         sys.exit(2)
-    
+
     # try to open the structure errors log file
     try:
-        log = open("./results/log.txt", "w") 
+        log = open("./results/log.txt", "w")
     except FileNotFoundError:
         os.mkdir("./results/") # create 'results' subdirectory
         log = open("./results/log.txt", "w")
-        
+
     all_loci = {} # return dictionary
     locus_id = "" # the current gene being analyzed
     mRNA_id = "" # the current mRNA being analyzed
     line_index = 1 # number of the file line currently read
     locus = lc.Locus() # Initialisation of the Locus class instance to construct
     loci = None
-    
+
     delim = re.compile("^[^,?;:/!$%@#~&\n\t]+")
-    
+
     for line in file:
-        
+
         parsed_line = line.split("\t")
-        
+
         if parsed_line[0] == "contig":
             continue
-        
-        if str(parsed_line[2]) == "gene": 
-            
+
+        if str(parsed_line[2]) == "gene":
+
             # if there was a previous gene, add it to return dictionary and
             # delete it. Then we create a new Locus instance
             # (if the gene is on the reverse strand, reverse its border list
             # before adding it to the dictionary)
             if loci != None:
-                
-                # if there was a previous gene, but its borders list is empty, 
+
+                # if there was a previous gene, but its borders list is empty,
                 # return an error
                 if locus_id != "" and locus.mRNAs[mRNA_id] == []:
                     print(f"\nLine {line_index} = get_gff_borders() function error : no coding sequence (CDS) could be found for the previous mRNA '{mRNA_id}' in file {path}\nit is possible the file has an incorrect features order. You can clean it using https://github.com/ranwez/GeneModelTransfer/blob/master/SCRIPT/VR/gff_cleaner.py\n")
                     sys.exit(1)
-                    
+
                 loci[locus_id] = locus
             locus = lc.Locus() # new instance for the locus being read
             mRNA_id = "" # identifier of the mRNA currently being read
-            parent_id = "" # identifier of the parent of the current structure 
+            parent_id = "" # identifier of the parent of the current structure
             locus_id = "" # identifier of the current locus
-        
+
         chrm = parsed_line[0] # name of the chromosome
         if chrm == "contig":
             continue
-        strand = "direct" if parsed_line[6] == "+" else "reverse" 
+        strand = "direct" if parsed_line[6] == "+" else "reverse"
         try:
             loci = all_loci[chrm+"_"+strand]
         except KeyError:
             all_loci[chrm+"_"+strand] = {}
             loci = all_loci[chrm+"_"+strand]
-        
-        # if we encounter a new gene which is not from a contig, 
+
+        # if we encounter a new gene which is not from a contig,
         # we get its information and create an instance in 'loci'
-        if str(parsed_line[2]) == "gene": 
-            
+        if str(parsed_line[2]) == "gene":
+
             locus.direction = strand
-            
+
             # we retrieve the id of the locus
             locus_id = get_structure_id(parsed_line, delim, verbose)
             locus.name = locus_id
-            
+
             # we retrieve the start and end coordinates of the locus
             start = int(parsed_line[3])
             locus.start = start # start coordinate of the locus
             end = int(parsed_line[4])
             locus.end = end # end coordinate of the locus
-            
+
             if verbose :
                 print("Reading the locus " + locus_id)
 
-        # if we encounter a new mRNA, we get its ID and create a key 
+        # if we encounter a new mRNA, we get its ID and create a key
         # in the instance's mRNAs attribute with an empty list
-        elif str(parsed_line[2]) == "mRNA": 
-            
+        elif str(parsed_line[2]) == "mRNA":
+
             mRNA_id = get_structure_id(parsed_line, delim, verbose)
             locus.mRNAs[mRNA_id] = [] # list of CDS coordinates of the mRNA
 
-        # if we encounter a CDS, we add its start and end positions to the 
+        # if we encounter a CDS, we add its start and end positions to the
         # corresponding mRNA key in the locus' mRNAs attribute
         elif str(parsed_line[2]) == exon_or_cds:
             parent_id = get_parent_id(parsed_line, delim, verbose)
-            
+
             if parent_id != mRNA_id:
-                print("\nIncorrect file structure (Parent of CDS is not previous mRNA) in file {path}. See 'log.txt' for more information")
+                print(f"\nIncorrect file structure (Parent of CDS is not previous mRNA) in file {path}. See 'log.txt' for more information")
                 log.write("Line " + str(line_index) + " : CDS parent ID (" + parent_id + ") does not match last mRNA ID (" + locus_id +")\n")
-                
+
             if mRNA_id == '':
                 print(f"\nLine {line_index} = get_gff_borders() function error : CDS has been found before any mRNA in file {path}\nit is possible the file has an incorrect features order. You can clean it using https://github.com/ranwez/GeneModelTransfer/blob/master/SCRIPT/VR/gff_cleaner.py\n")
                 sys.exit(1)
-            
+
             locus.mRNAs[mRNA_id].append(int(parsed_line[3]))
             locus.mRNAs[mRNA_id].append(int(parsed_line[4]))
-                
+
         line_index += 1 # increment the line indicator (for debug purposes)
-                
+
     if locus_id == "": # if the locus_id still has default value, return error
         print(f"\nget_gff_borders() function error : no locus could be found in file '{path}'\n")
         sys.exit(1)
-        
+
     file.close()
     log.close()
     loci[locus_id] = locus # add the last locus to the return dictionary
-    
+
     # we return the entire dictionary with all loci (of all chromosomes)
     return all_loci
